08140e8f0b9b484aaaf2b98bd8737da205f7936f
MarioSantoro <mariosantoro124@gmail.com>
2023-05-17 14:53:56 +0200

    add bonus for exercise Mail

--- Mail/Js/myscript.js
+++ Mail/Js/myscript.js
@@ -1,14 +1,20 @@
 let vet1 = ["riccardopetricca208@gmail.com" , "stefanocappellini118@gmail.com" , "antoniosagoleo777@gmail.com"]
-let result = false;
-let mail = prompt("Inserisci la tua mail per accedere");
-   for(let i=0; i<vet1.length;i++){
-      if(mail === vet1[i]){
-         result =true;
-         i= vet1.length;
-      }
-   }
-   if(result==true){
-    document.getElementById("login").innerHTML = "Benvenuto :" + mail;
-   }else{
-    document.getElementById("login").innerHTML = "Accesso non consentito!! Motivo: non è stata trovata la tua mail!!";
-   }
+
+let btn = document.getElementById("sumbit");
+btn.addEventListener("click" , 
+    function(){
+        let result = false;
+        let mail = document.getElementById("floatingInput").value;
+        for(let i=0; i<vet1.length;i++){
+            if(mail === vet1[i]){
+                result =true;
+                i= vet1.length;
+            }
+        }
+        if(result==true){
+            document.getElementById("output").innerHTML = "Benvenuto : " + mail;
+        }else{
+            document.getElementById("output").innerHTML = "Accesso non consentito!! Motivo: non è stata trovata la tua mail!!";
+        }
+    }
+)
